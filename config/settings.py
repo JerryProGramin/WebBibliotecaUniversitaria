8bbde266bb879f5fca464d06d3c0faec70e2969a
kory <koryna654@gmail.com>
2025-11-13 00:53:28 -0500

    Api de google y de github para iniciar sesión, pruebas

--- config/settings.py
+++ config/settings.py
@@ -119,17 +119,32 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'oct07cdo_biblioteca_bd',      
+#         'USER': 'oct07cdo_dext',
+#         'PASSWORD': 'IrJn}0GlGm4ul,%U',
+#         'HOST': 'codeinfiniter.com',          
+#         'PORT': '3306',                
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'oct07cdo_biblioteca_bd',      
-        'USER': 'oct07cdo_dext',
-        'PASSWORD': 'IrJn}0GlGm4ul,%U',
-        'HOST': 'codeinfiniter.com',          
+        'NAME': 'biblioteca_bd',      
+        'USER': 'root',               
+        'PASSWORD': '',               
+        'HOST': '127.0.0.1',          
         'PORT': '3306',                
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
